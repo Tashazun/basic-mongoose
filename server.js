f56564a6b2f30e2907bf33ee745ac00b4bfa0022
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-17 20:17:05 -0700

    basic structure set up

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const http = require('http');
-const app = require()
+const app = require('../lib/app');
+const connect = require('../lib/connect');
+
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/movieReel';
+
+connect(MONGODB_URI);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log('What lies behind us and what lies before us are tiny matters compared to what lies within us.');
+});
